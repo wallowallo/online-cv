1b8a86894d799e4961caa097e8bde13a829ffb2a
Krister <krister.torsvik@gmail.com>
2017-02-04 13:37:57 +0100

    added some styling and a footer

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import '../_helpers/rxjs-operators';
